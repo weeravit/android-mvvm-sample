3f6b371ce698cfcf1bb17e55ae7e74ace6a814cd
Weeravit Sirita <unlockablez@gmail.com>
2016-09-10 15:56:43 +0700

    add samplefragment into mainactivity

--- app/src/main/java/com/weeravit/androidmvvm/MainActivity.java
+++ app/src/main/java/com/weeravit/androidmvvm/MainActivity.java
@@ -9,6 +9,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            initInstances();
+        }
+    }
+
+    private void initInstances() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.contentContainer, SampleFragment.newInstance())
+                .commit();
     }
 
 }
